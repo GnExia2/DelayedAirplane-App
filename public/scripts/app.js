acaff0b167a4bc05e62666685b271d1eaa536b0d
YOUR_GITHUB_USERNAME <garrickdark@yahoo.com>
2017-07-25 15:59:22 -0700

    created schema and sanity check

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
   console.log('sanity check');
-
+  // $.ajax({
+  // method: 'GET',
+  // url: '/api/delays',
+  // success: handleSuccess,
+  // error: handleError
 });
+
+// function handleSuccess(data){
+//   console.log(res.json(data))
+// };
+//
+// function handleError(err){
+//   console.log('There has been an error: ', err);
+// }

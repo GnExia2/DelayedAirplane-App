2e6d97cc342533a65e216b12923d12704fad7d1a
ryan-barrett <ascending62@gmail.com>
2017-07-25 16:19:32 -0700

    typo fixes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,19 +1,16 @@
 $(document).ready(function(){
   console.log('sanity check');
-<<<<<<< HEAD
   $(".dropdown-toggle").dropdown();
 
-});
-=======
 
-  // $.ajax({
-  // method: 'GET',
-  // url: '/api/delays',
-  // success: handleSuccess,
-  // error: handleError
-
+// $.ajax({
+// method: 'GET',
+// url: '/api/delays',
+// success: handleSuccess,
+// error: handleError
 
 });
+
 
 // function handleSuccess(data){
 //   console.log(res.json(data))
@@ -22,4 +19,3 @@ $(document).ready(function(){
 // function handleError(err){
 //   console.log('There has been an error: ', err);
 // }
->>>>>>> fa9023b7a1bb242815884da9dffc1bfd68f76983

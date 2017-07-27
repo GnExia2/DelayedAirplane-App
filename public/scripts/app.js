026d07fd0d94abf69734cc5cd62c78a402f0b8db
ryan-barrett <ascending62@gmail.com>
2017-07-27 12:22:02 -0700

    added form post functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,6 @@
+var allDelays = [];
+
+
 $(document).ready(function(){
   $(".dropdown-toggle").dropdown();
 
@@ -7,31 +10,35 @@ $(document).ready(function(){
     success: handleSuccess,
     error: handleError
   });
-});
-
-$('#delay-form form').on('submit', function(e) {
-  e.preventDefault();
-  var formData = $(this).serialize();
-  console.log('formData', formData);
-  $.post('/api/delays', formData, function(delay) {
-    console.log('album after POST', delay);
-    renderDelay(delay);
+  $('#delay-form form').on('submit', function(e) {
+    e.preventDefault();
+    var formData = $(this).serialize();
+    console.log('formData', formData);
+    $.post('/api/delays', formData, function(delay) {
+      console.log('flight after POST', delay);
+      renderDelay(delay);  //render the server's response
+    });
+    $(this).trigger("reset");
   });
-  $(this).trigger("reset");
+
+
 });
 
 // catch and handle the click on an add song button
-$('#albums').on('click', '.add-song', handleAddDelayClick);
+// $('#albums').on('click', '.add-song', handleAddDelayClick);
 
 // save song modal save button
- $('#saveSong').on('click', handleNewSongSubmit);
+ // $('#saveSong').on('click', handleNewSongSubmit);
  // $('#albums').on('click', '.edit-songs', handleEditSongsClick);
 
-  function handleSuccess(data){
-    data.forEach(function(i){
+  function handleSuccess(success){
+    success.forEach(function(i){
       renderDelay(i);
     });
 };
+
+
+
 
   function handleError(err){
     console.log('There has been an error: ', err);
@@ -108,48 +115,48 @@ $('#flights').on('click', '.edit-delay', function(e) {
   $(selectorIdEditDelay).css("display","none");
 
 
-  // when the delay modal submit button is clicked:
-function handleNewSongSubmit(e) {
-  e.preventDefault();
-  var $modal = $('#flightModal');
-  var $airlineNameField = $modal.find('#airline');
-  var $flightNumberField = $modal.find('#flightNumber');
-
-  // get data from modal fields
-  // note the server expects the keys to be 'airline', 'flightNumber' so we use those.
-  var dataToPost = {
-    airline: $airlineNameField.val(),
-    flightNumber: $flightNumberField.val()
-  };
-  var delayId = $modal.data('albumId');
-  console.log('retrieved delay:')
-  // POST to SERVER
-  var delayPostToServerUrl = '/api/delays/'+ delayId;
-  $.post(songPostToServerUrl, dataToPost, function(data) {
-    console.log('received data from post:', data);
-    // clear form
-    $airlineNameField.val('');
-    $flightNumberField.val('');
-  });
-    // close modal
-    $modal.modal('hide');
-    // update the correct album to show the new song
-    $.get('/api/delays/' + delayId, function(data) {
-      // remove the current instance of the album from the page
-      $('[data-delay-id=' + delayId + ']').remove();
-      // re-render it with the new album data (including songs)
-      renderDelay(data);
-    });
-  };error(function(err) {
-    console.log('post error', err);
-  });
-  // when the add song button is clicked, display the modal
-function handleAddDelayClick(e) {
-  console.log('add-delay clicked!');
-  var currentDelayId = $(this).closest('.delay').data('delay-id'); // "5665ff1678209c64e51b4e7b"
-  console.log('id',currentDelayId);
-  $('#flightModal').data('delay-id', currentDelayId);
-  $('#flightModal').modal();  // display the modal!
-}
+//   // when the delay modal submit button is clicked:
+// function handleNewSongSubmit(e) {
+//   e.preventDefault();
+//   var $modal = $('#flightModal');
+//   var $airlineNameField = $modal.find('#airline');
+//   var $flightNumberField = $modal.find('#flightNumber');
+//
+//   // get data from modal fields
+//   // note the server expects the keys to be 'airline', 'flightNumber' so we use those.
+//   var dataToPost = {
+//     airline: $airlineNameField.val(),
+//     flightNumber: $flightNumberField.val()
+//   };
+//   var delayId = $modal.data('albumId');
+//   console.log('retrieved delay:')
+//   // POST to SERVER
+//   var delayPostToServerUrl = '/api/delays/'+ delayId;
+//   $.post(songPostToServerUrl, dataToPost, function(data) {
+//     console.log('received data from post:', data);
+//     // clear form
+//     $airlineNameField.val('');
+//     $flightNumberField.val('');
+//   });
+//     // close modal
+//     $modal.modal('hide');
+//     // update the correct album to show the new song
+//     $.get('/api/delays/' + delayId, function(data) {
+//       // remove the current instance of the album from the page
+//       $('[data-delay-id=' + delayId + ']').remove();
+//       // re-render it with the new album data (including songs)
+//       renderDelay(data);
+//     });
+//   };error(function(err) {
+//     console.log('post error', err);
+//   });
+//   // when the add song button is clicked, display the modal
+// function handleAddDelayClick(e) {
+//   console.log('add-delay clicked!');
+//   var currentDelayId = $(this).closest('.delay').data('delay-id'); // "5665ff1678209c64e51b4e7b"
+//   console.log('id',currentDelayId);
+//   $('#flightModal').data('delay-id', currentDelayId);
+//   $('#flightModal').modal();  // display the modal!
+// }
 
 });

f2a12ce23b1c9d5b8c728a9166080163736b1aa9
YOUR_GITHUB_USERNAME <garrickdark@yahoo.com>
2017-07-27 15:04:28 -0700

    fixed issue where seed.js was not seeding correctly

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -3,8 +3,11 @@ var db = require('../models');
 function index(req, res) {
   db.comment.find({}, function(err, allComments){
     res.json(allComments);
-  })
-};
+
+
+  });
+}
+
 
 module.exports = {
   index: index,

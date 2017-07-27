fd01f3ffb2c0014df1fd1bbfdd7fafa7db92edcc
ryan-barrett <ascending62@gmail.com>
2017-07-27 13:55:19 -0700

    fixed post error

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -3,8 +3,9 @@ var db = require('../models');
 function index(req, res) {
   db.comments.find({}, function(err, allComments){
     res.json(allComments);
-  })
 
+  });
+}
 
 module.exports = {
   index: index,
